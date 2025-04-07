d109533e9de7f75348abb1bb2947f0defd2c1557
VictorPresenca <presenca.victor@gmail.com>
2025-04-07 20:18:50 -0300

    primeira atualização na pagina de pagamento

--- src/app/pagamento/pagamento.page.ts
+++ src/app/pagamento/pagamento.page.ts
@@ -13,4 +13,34 @@ export class PagamentoPage implements OnInit {
   ngOnInit() {
   }
 
+  valor: string = 'R$0,00';
+
+formatarMoeda(event: any) {
+  let valorDigitado = event.detail.value;
+
+  // Remove tudo que não for número
+  let valorNumerico = valorDigitado.replace(/\D/g, '');
+
+  // Converte para centavos e formata
+  let valorFormatado = (parseFloat(valorNumerico) / 100).toLocaleString('pt-BR', {
+    style: 'currency',
+    currency: 'BRL'
+  });
+
+  this.valor = valorFormatado;
+}
+
+formasPagamento: string[] = ['Crédito', 'Débito', 'Dinheiro', 'PIX'];
+formaSelecionada: string = '';
+
+selecionarForma(forma: string) {
+  if (this.formaSelecionada === forma) {
+    // Desmarca se clicar de novo
+    this.formaSelecionada = '';
+  } else {
+    // Seleciona nova forma
+    this.formaSelecionada = forma;
+  }
+}
+
 }
