d8621a80b4c43349627480e6ba388d60167da9d3
VictorPresenca <presenca.victor@gmail.com>
2025-05-11 21:35:48 -0300

    att cardapio para listagem funcionar

--- src/app/cardapio/cardapio.page.ts
+++ src/app/cardapio/cardapio.page.ts
@@ -1,9 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-<<<<<<< HEAD
 import { HttpClient, HttpHeaders } from '@angular/common/http';
-=======
-import { ColetaBackendService } from '../services/coleta-backend.service';
->>>>>>> 44c188b16d3123c5c531d2f7e3c180edaa90b28f
 
 @Component({
   selector: 'app-cardapio',
@@ -12,29 +8,12 @@ import { ColetaBackendService } from '../services/coleta-backend.service';
   standalone: false,
 })
 export class CardapioPage implements OnInit {
-<<<<<<< HEAD
 
   solicitacoes: any[] = [];
 
   constructor(private http: HttpClient) { }
 
-  ngOnInit() {
-    this.carregarSolicitacoes();
-=======
-  solicitacoes: any[] | undefined;
-  constructor(private coleta: ColetaBackendService) { }
-
-  ngOnInit() {
-    this.coleta.listarSolicitacoes(1, 5).subscribe({
-      next: (valor) => {
-        this.solicitacoes = valor.data;
-      },
-      error(err) {
-        console.log(err)
-      },
-    })
->>>>>>> 44c188b16d3123c5c531d2f7e3c180edaa90b28f
-  }
+  ngOnInit() {}
 
 
   carregarSolicitacoes(){
