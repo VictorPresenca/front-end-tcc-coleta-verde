e29ed5280babb87c42fa797dd4b782e76b96afd2
aluno <etecae023@hotmail.com>
2025-05-28 20:29:13 -0300

    Telas configurações

--- src/app/version-settings/version-settings.page.ts
+++ src/app/version-settings/version-settings.page.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-version-settings',
@@ -6,11 +6,4 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./version-settings.page.scss'],
   standalone: false
 })
-export class VersionSettingsPage implements OnInit {
-
-  constructor() { }
-
-  ngOnInit() {
-  }
-
-}
+export class VersionSettingsPage {}
