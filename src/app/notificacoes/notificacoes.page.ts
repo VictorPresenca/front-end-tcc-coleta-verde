11997502454f29d7b31c949230b762510b4f2815
aluno <etecae023@hotmail.com>
2025-06-13 21:47:05 -0300

    Ajustes em Notificações

--- src/app/notificacoes/notificacoes.page.ts
+++ src/app/notificacoes/notificacoes.page.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { NotificacoesService } from '../services/notificacoes.service';
-
+import { ColetaBackendService, EColetaRole } from '../services/coleta-backend.service';
+import { Router } from '@angular/router';
+import { lastValueFrom } from 'rxjs';
 
 @Component({
   selector: 'app-notificacoes',
@@ -12,7 +14,11 @@ export class NotificacoesPage implements OnInit {
 
   notificacoes: any[] = [];
 
-  constructor(private notificacoesService: NotificacoesService) { }
+  constructor(
+    private notificacoesService: NotificacoesService,
+    private coletaBackendService: ColetaBackendService,
+    private router: Router
+  ) { }
 
   ngOnInit() {
     this.notificacoesService.notificacoes$.subscribe((dados: any[]) => {
@@ -23,5 +29,17 @@ export class NotificacoesPage implements OnInit {
   atualizar() {
     this.notificacoesService.forcarAtualizacao().subscribe();
   }
+
+  async cliqueNotificao() {
+    const userResponse = await lastValueFrom(this.coletaBackendService.getCurrentUserData());
+    const role = userResponse.data?.role;
+
+    if (role === EColetaRole.enterprise) {
+      this.router.navigate(['/pedidos-cliente']);
+    } else if (role === EColetaRole.employee) {
+      this.router.navigate(['/pedidos-prestador']);
+    }
+  }
+
 
 }
