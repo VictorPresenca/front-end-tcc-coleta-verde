3f7697ede2e0fd752ade24509f3720c6b322876c
aluno <etecae023@hotmail.com>
2025-06-16 20:26:43 -0300

    finalizar prestador

--- src/app/finalizar-pedido-prestador/finalizar-pedido-prestador.page.ts
+++ src/app/finalizar-pedido-prestador/finalizar-pedido-prestador.page.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
-import { NavController } from '@ionic/angular';
+import { NavController, ToastController } from '@ionic/angular';
+import { ColetaBackendService } from '../services/coleta-backend.service';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'app-finalizar-pedido-prestador',
@@ -9,12 +11,43 @@ import { NavController } from '@ionic/angular';
 })
 export class FinalizarPedidoPrestadorPage implements OnInit {
 
-  constructor(private navCtrl: NavController) {}
+  solicitationId!: number;
 
-  ngOnInit() {}
+  constructor(private toastCtrl: ToastController, private router: Router, private route:ActivatedRoute, private back: ColetaBackendService) {}
+
+  ngOnInit() {
+    this.solicitationId = Number(this.route.snapshot.paramMap.get('id'));
+  }
+
+  finalizar(){
+    this.back.finalizarSolicitacao(this.solicitationId).subscribe({
+      next: async (res) => {
+        console.log('solicitação finalizada com sucessoo! ', this.solicitationId);
+        if (res.status === 200) {
+          const toast = await this.toastCtrl.create({
+            message: 'Solicitação finalizada com sucesso!',
+            duration: 2000,
+            color: 'success'
+          });
+          await toast.present();
+            this.router.navigateByUrl('/home-prestador');
+          } else {
+                const toast = await this.toastCtrl.create({
+            message: 'Erro ao finalizar solcitação!',
+            duration: 2000,
+            color: 'danger'
+          });
+          await toast.present();
+          }
+        error: () => {
+        console.log('Erro de conexão com o servidor');
+        }
+  }
+})
+  }
 
   cancelar() {
-    this.navCtrl.navigateRoot('/pedidos-prestador');
+    this.router.navigateByUrl('/pedidos-prestador');
   }
 }
 
